fb500833bb31000ce1ba6ace94830d12d64e7caf
Alexandre LE CORRE <a.elreco@gmail.com>
2021-04-30 12:18:30 +0200

    :construction:

--- cloud/fortniteapi-io/news.js
+++ cloud/fortniteapi-io/news.js
@@ -3,7 +3,7 @@ const getImage = require('../function/get-image.js')
 
 Parse.Cloud.beforeFind("News", async (req) => {
     if (!req.master) {
-        const apiNews = await fortniteAPI.getNews("br");
+        const apiNews = await fortniteAPI.getNews("br").catch(() => {});
         if (apiNews.result) {
             await Promise.all(apiNews.news.map(async (n) => {
                 const newsQuery = new Parse.Query("News");
@@ -24,12 +24,14 @@ async function createNews(n) {
     const news = new News();
     news.set("apiId", n.id);
     news.set("title", n.title);
-    news.set("subtitle", n.tabTitle);
+    if (n.tabTitle.localeCompare(n.title) !== 0) {
+        news.set("subtitle", n.tabTitle);
+    }
     news.set("text", n.body);
     news.set("adspace", n.adspace);
-    news.set("createdAt", n.date);
+    news.set("date", new Date(n.date));
     news.set("video", n.video);
-    news.set("type", "br");
+    news.set("type", "normal");
     if (n.image) {
         const file = await getImage(n.image);
         news.set("image", file);
