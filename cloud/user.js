68f7a187fc2186cfe59882db2093d5c51cfc82ae
Alexandre LE CORRE <a.elreco@gmail.com>
2021-05-06 22:40:23 +0200

    :construction:

--- cloud/user.js
+++ cloud/user.js
@@ -3,9 +3,10 @@ Parse.Cloud.beforeSave(Parse.User, () => { },
 {
     fields: {
         password: {
-            required: true,
             options: val => {
-                return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(val)
+                if (val) {
+                    return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(val)
+                }
             },
             error: 'Votre mot de passe doit contenir au moins 8 caractères, une minuscule, une majuscule et un caractère spécial'
           }
