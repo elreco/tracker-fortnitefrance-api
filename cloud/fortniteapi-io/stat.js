6874371cda87b8ea423134b0bb22234151738612
Alexandre LE CORRE <a.elreco@gmail.com>
2021-05-14 21:45:38 +0200

    ladder job

--- cloud/fortniteapi-io/stat.js
+++ cloud/fortniteapi-io/stat.js
@@ -52,6 +52,7 @@ async function createOrUpdateStat(s, accountId, stat = null) {
   stat.set("season", s.season);
   stat.set("date", new Date());
   stat.set("totalKd", getTotalKd(s.global_stats));
+  stat.set("totalWins", getTotalWins(s.global_stats));
   if (!stat.get('character')) {
     const dirPath = path.resolve(__dirname, '../assets/characters/');
     const file = await getRandomLocalImage(dirPath);
@@ -60,63 +61,28 @@ async function createOrUpdateStat(s, accountId, stat = null) {
   await stat.save(null, {
     useMasterKey: true
   })
-  /* await setRank(stat) */
+
   await createMatchesFromApi(stat);
 
   return stat;
 }
 
-async function setRank(stat) {
-  const kd = getTotalKd(stat.get('global_stats'))
-
+async function setLadder() {
   const statQuery = new Parse.Query("Stat");
-  statQuery.ascending("totalKd");
-  statQuery.greaterThanOrEqualTo('totalKd', kd);
-  statQuery.notEqualTo('objectId', stat.id);
-  const statReplace = await statQuery.first({
+  statQuery.descending("totalKd", "totalWins");
+  statQuery.select("rank");
+  const stats = await statQuery.find({
     useMasterKey: true
   })
-  if (statReplace && statReplace.get('rank') > 300) return;
-  if (statReplace) {
-    const rank = statReplace.get('rank')
-    stat.set('rank', rank + 1)
-    const statQuery2 = new Parse.Query("Stat");
-    statQuery2.descending("rank");
-    statQuery2.greaterThan('rank', rank);
-    statQuery2.notEqualTo('objectId', stat.id);
-    if (rank != stat.get('rank')) {
-      const allStatBefore = await statQuery2.find({
-        useMasterKey: true
-      })
-      await Promise.all(allStatBefore.map(async (s) => {
-        s.set('rank', s.get('rank') + 1)
-        await s.save(null, {
-          useMasterKey: true
-        })
-      }))
-    }
 
-  } else {
-    if (stat.get('rank') != 1) {
-      stat.set('rank', 1)
-      const statQuery2 = new Parse.Query("Stat");
-      const allStatBefore = await statQuery2.find({
-        useMasterKey: true
-      })
-      await Promise.all(allStatBefore.map(async (s) => {
-        s.set('rank', s.get('rank') + 1)
-        await s.save(null, {
-          useMasterKey: true
-        })
-      }))
-    }
-  }
-
-  if (statReplace) {
-    await stat.save(null, {
+  var rank = 0;
+  await Promise.all(stats.map(async (s) => {
+    rank += 1;
+    s.set('rank', rank);
+    await s.save(null, {
       useMasterKey: true
     })
-  }
+  }))
 }
 
 function getTotalKd(global_stats) {
@@ -136,4 +102,21 @@ function getTotalKd(global_stats) {
   return totalKd;
 }
 
-module.exports = { getStatFromApi }
+function getTotalWins(global_stats) {
+  let wins = 0;
+
+  if (global_stats.solo && global_stats.solo.placetop1) {
+    wins += global_stats.solo.placetop1
+  }
+
+  if (global_stats.duo && global_stats.duo.placetop1) {
+    wins += global_stats.duo.placetop1
+  }
+
+  if (global_stats.squad && global_stats.squad.placetop1) {
+    wins += global_stats.squad.placetop1
+  }
+  return wins;
+}
+
+module.exports = { getStatFromApi, setLadder }
--- cloud/stat.js
+++ cloud/stat.js
@@ -1,5 +1,6 @@
 const {
-  getStatFromApi
+  getStatFromApi,
+  setLadder
 } = require('./fortniteapi-io/stat.js')
 
 Parse.Cloud.beforeFind("Stat", async (req) => {
@@ -14,4 +15,13 @@ Parse.Cloud.beforeFind("Stat", async (req) => {
 }, {
   requireMaster: false,
   requireUser: false
+});
+
+Parse.Cloud.job("ladder", async (req) => {
+  const {
+    message
+  } = req;
+  message("Ladder update started");
+  await setLadder()
+  message("Ladder update finished");
 });
